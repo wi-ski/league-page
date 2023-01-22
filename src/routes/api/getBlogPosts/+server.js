61f57cbeefdac4a4f2dde957020a0dd85d86d7d7
Will Dembinski <willdembinski@gmail.com>
2023-01-21 16:23:09 -0800

    Configs and first img

--- src/routes/api/getBlogPosts/+server.js
+++ src/routes/api/getBlogPosts/+server.js
@@ -2,7 +2,7 @@ import contentful from 'contentful';
 import { json, error } from '@sveltejs/kit';
 
 export async function GET() {
-    if(!import.meta.env.VITE_CONTENTFUL_CLIENT_ACCESS_TOKEN) {
+    if (!import.meta.env.VITE_CONTENTFUL_CLIENT_ACCESS_TOKEN) {
         throw error(500, "Missing VITE_CONTENTFUL_CLIENT_ACCESS_TOKEN (added dependency in v2.0), go to https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#iii-add-a-blog for directions to add it");
     }
     const client = contentful.createClient({
@@ -11,8 +11,10 @@ export async function GET() {
         // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
         accessToken: import.meta.env.VITE_CONTENTFUL_CLIENT_ACCESS_TOKEN
     });
-	const data = await client.getEntries({content_type: 'blog_post'})
-        .catch(e=> {
+    // See: https://app.contentful.com/spaces/kpcv68qeirfe/content_types/dfflLeagueContentTypeDefault/fields
+    // Note: Cant rever this value.
+    const data = await client.getEntries({ content_type: 'dfflLeagueContentTypeDefault' })
+        .catch(e => {
             console.error(e);
             throw error(500, "Problem retrieving blog posts");
         });
